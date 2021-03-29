e030898eae7b32aa014f0e7827d0897f4501e723
Grzegorz Dubiel <greg2012201@gmail.com>
2021-03-29 10:37:34 +0200

    Add basic styles for the Slider

--- src/js/createSlider.js
+++ src/js/createSlider.js
@@ -7,20 +7,40 @@ export class SliderCreator {
 
   createElements() {
     const mask = document.createElement('div');
-    const span = document.createElement('span');
-
-    this.appendToFragment([div, span]);
+    const imageContainer = document.createElement('img');
+    const output = [mask, imageContainer];
+    this.elemntsStyle([{mask, imageContainer}]);
+    this.appendToFragment(output);
   }
   appendToFragment(elements) {
     elements.map((element) => {
       this.fragment.appendChild(element);
     });
   }
-  elemntsStyle() {}
-  render() {
-    const span = this.fragment.querySelector('span');
+  elemntsStyle(elements) {
+    elements.map(({mask, imageContainer}) => {
+      mask.style.position = 'fixed';
+      mask.style.display = 'grid';
+      mask.style.gridTemplateRows = '90% 1fr';
+      mask.style.gridTemplateColumns = '15% 1fr 15%';
+      mask.style.alignItems = 'center';
+      mask.style.left = '0';
+      mask.style.top = '0';
+      mask.style.height = '100vh';
+      mask.style.width = '100%';
+      mask.style.backgroundColor = 'rgba(0,0,0,0.20)';
 
-    document.body.appendChild(fragment);
+      imageContainer.style.height = '70%';
+      imageContainer.style.width = '100%';
+      imageContainer.style.gridRow = '1/2';
+      imageContainer.style.gridColumn = '2/3';
+    });
+  }
+  render() {
+    const mask = this.fragment.querySelector('div');
+    const imageContainer = this.fragment.querySelector('img');
+    mask.appendChild(imageContainer);
+    document.body.appendChild(mask);
   }
   deleteElements() {}
 }
