19225f397db047a7f9ecb0ca9911dce33e18f223
Grzegorz Dubiel <greg2012201@gmail.com>
2021-04-01 09:10:34 +0200

    Add navigationBar and closeButton

--- src/js/SliderCreator.js
+++ src/js/SliderCreator.js
@@ -3,7 +3,6 @@ import {SliderStyle} from './SliderStyle';
 export class SliderCreator {
   constructor(numberOfImages) {
     this.fragment = new DocumentFragment();
-    console.log(numberOfImages);
     this.numberOfImages = parseInt(numberOfImages, 10);
     this.sliderStyle = new SliderStyle();
     this.createElements();
@@ -13,29 +12,67 @@ export class SliderCreator {
   createElements() {
     const sliderContainer = document.createElement('div');
     const imageContainer = document.createElement('div');
+    const closeButton = document.createElement('button');
+    const navigationBar = document.createElement('div');
     const imagesFields = this.createImagesFields();
     const paginationButtons = this.createPaginationButtons();
-    const output = [{sliderContainer, imageContainer, imagesFields, paginationButtons}];
+    const output = [
+      {
+        sliderContainer,
+        imageContainer,
+        imagesFields,
+        paginationButtons,
+        navigationBar,
+        closeButton,
+      },
+    ];
     this.createElementsStructure(output);
     this.addClasses(output);
     this.sliderStyle.setElemntsStyle(output);
   }
   createElementsStructure(elements) {
-    elements.flat().map(({sliderContainer, imageContainer, imagesFields, paginationButtons}) => {
-      this.append([imagesFields], imageContainer);
-      this.append([imageContainer, paginationButtons], sliderContainer);
-      this.append([sliderContainer], this.fragment);
-    });
+    elements
+      .flat()
+      .map(
+        ({
+          sliderContainer,
+          imageContainer,
+          imagesFields,
+          paginationButtons,
+          navigationBar,
+          closeButton,
+        }) => {
+          this.append([imagesFields], imageContainer);
+          this.append(
+            [imageContainer, paginationButtons, navigationBar, closeButton],
+            sliderContainer
+          );
+          this.append([sliderContainer], this.fragment);
+        }
+      );
   }
   addClasses(elements) {
-    elements.flat().map(({sliderContainer, imageContainer, imagesFields, paginationButtons}) => {
-      sliderContainer.classList.add('slider-container');
-      imageContainer.classList.add('image-container');
-      imagesFields.map((field) => field.classList.add('images-fields'));
-      const [leftButton, rightButton] = paginationButtons;
-      leftButton.classList.add('pagination-button', 'left');
-      rightButton.classList.add('pagination-button', 'right');
-    });
+    elements
+      .flat()
+      .map(
+        ({
+          sliderContainer,
+          imageContainer,
+          imagesFields,
+          paginationButtons,
+          navigationBar,
+          closeButton,
+        }) => {
+          sliderContainer.classList.add('slider-container');
+          imageContainer.classList.add('image-container');
+          imagesFields.map((field) => field.classList.add('images-fields'));
+          const [leftButton, rightButton] = paginationButtons;
+          leftButton.classList.add('pagination-button', 'left');
+          rightButton.classList.add('pagination-button', 'right');
+          navigationBar.classList.add('navigation-bar');
+          closeButton.classList.add('close-button');
+        }
+      );
   }
   createImagesFields() {
     let output = [];
@@ -48,24 +85,21 @@ export class SliderCreator {
 
     return output;
   }
-  append(children, parent) {
-    children.flat().map((child) => {
-      parent.appendChild(child);
-    });
-  }
+
   createPaginationButtons() {
     let buttons = [];
     for (let i = 0; i < 2; i++) {
       const button = document.createElement('button');
-      const arrow = document.createElement('span');
-
-      this.append([arrow], button);
-
       buttons.push(button);
     }
     return buttons;
   }
 
+  append(children, parent) {
+    children.flat().map((child) => {
+      parent.appendChild(child);
+    });
+  }
   render() {
     document.body.appendChild(this.fragment);
   }
